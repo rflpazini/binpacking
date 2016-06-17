93abaf4f279868cdfa69e6204317452a290899ba
rflpazini <rflpazini@gmail.com>
2016-06-17 13:53:29 -0300

    Add new files

---
+++ assets/js/packing.js
@@ -0,0 +1,32 @@
+class Packing {
+    constructor(width, height) {
+        this.root = { x: 0, y: 0, w: width, h: height };
+    }
+
+    fit(blocks) {
+        var n, node, block;
+        for (n = 0; n < blocks.length; n++) {
+            block = blocks[n];
+            if (node = this.findNode(this.root, block.w, block.h)) {
+                block.fit = this.splitNode(node, block.w, block.h);
+            }
+        }
+    }
+
+    findNode(root, width, height) {
+        if (root.used) {
+            return this.findNode(root.right, width, height) || this.findNode(root.down, width, height);
+        } else if ((width <= root.w) && (height <= root.h)) {
+            return root;
+        } else {
+            return null;
+        }
+    }
+
+    splitNode(node, width, height) {
+        node.used = true;
+        node.down = { x: node.x, y: node.y + height, w: node.w, h: node.h - height };
+        node.right = { x: node.x + width, y: node.y, w: node.w - width, h: height };
+        return node;
+    }
+}
